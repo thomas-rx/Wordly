efd2445986d044946c41a686f3eae4b3446186f5
Thomas R <contact@xrths.fr>
2022-03-23 18:58:46 +0100

    ⚡ Improved word choice

--- src/js/word.js
+++ src/js/word.js
@@ -5,30 +5,22 @@ import { Buffer } from 'buffer';
 class DailyWord {
     constructor() {
         this.date = new Date().toISOString().slice(0, 10);
-        this.seed = parseInt(seedrandom(this.date).quick() * 10000);
-        this.wordList = fs.readFileSync('src/data/mots.txt').toString().split('\n')
+        this.seed = parseInt(seedrandom(this.date).quick() * 1000);
+        this.dictionary = fs.readFileSync('src/data/dictionnaire.txt').toString().split('\n');
+        this.production = fs.readFileSync('src/data/production.txt').toString().split('\n');
     }
 
-    getWord() {
-        var w = this.wordList[this.seed];
-        let valid = false;
-        while (w.length < 4 || w.length > 6 || !valid) {
-            w = this.wordList[this.seed];
-            if ((w.endsWith("IR") || w.endsWith("ER"))
-                && (!w.endsWith("S") && !w.endsWith("Z") && !w.endsWith("ES") && !w.endsWith("EE") && !w.endsWith("ER"))
-                && (!w.startsWith("K") && !w.startsWith("Z") && !w.startsWith("X"))
-            ) {
-                break;
-            } else {
-                this.seed += 50;
-            }
-        }
-        console.log(this.seed);
-        return w.toLowerCase();
+    getDictionary() {
+        return this.dictionary.filter(w => w.length == 5);
     }
 
-    getWordList() {
-        return this.wordList;
+    getProduction() {
+        return this.production.filter(w => w.length == 5 && w.match(/[A-Z]/));
+    }
+
+    getRandomWord() {
+        const productionDictionary = this.getProduction();
+        return productionDictionary[this.seed];
     }
 }
 
